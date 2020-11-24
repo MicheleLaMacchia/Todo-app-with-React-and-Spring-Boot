0d043316e221a6e5f9793de3cc90f2ee2138ebc3
MicheleLaMacchia <66997236+MicheleLaMacchia@users.noreply.github.com>
2020-11-24 12:46:27 +0100

    commit

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      hello
+      <FirstComponent/>
     </div>
   );
 }
+
+const FirstComponent = () => {
+  return ( <div>
+    First component
+  </div>  );
+}
+ 
 
 export default App;
